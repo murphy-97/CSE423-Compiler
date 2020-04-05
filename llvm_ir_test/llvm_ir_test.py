3fa1ef23aa6b5facebbc2a851bfa3661322d098a
Hannah Murphy <han.jan.murphy@gmail.com>
2020-04-05 16:30:19 -0600

    IR builder handles function definitions, function calls, and arithmetic

--- llvm_ir_test/llvm_ir_test.py
+++ llvm_ir_test/llvm_ir_test.py
@@ -38,6 +38,8 @@ def build_llvm(ast):
         "void":   ir.VoidType()
     }
 
+    ir_funcs = {}
+
     # Create module
     module = ir.Module(name="program")
 
@@ -91,15 +93,174 @@ def build_llvm(ast):
             )
 
             function = ir.Function(module, func_type, name=func_name)
+            ir_funcs[func_name] = function
 
             # Build function
             block = function.append_basic_block(name="entry")
             builder = ir.IRBuilder(block)
 
             # FUNCTION IMPLEMENTATION BUILT HERE
+            traversal_stack = []    # Used to build node_stack
+            node_stack = []         # Used to build function
+            traversal_stack.append(func_body)
 
-            # PLACEHOLDER - RETURNS WITHOUT A VALUE
-            builder.ret_void()
+            while (len(traversal_stack) > 0):
+                iter_node = traversal_stack.pop()
+                node_stack.append(iter_node)
+                for child in ast.children(iter_node.identifier):
+                    traversal_stack.append(child)
+
+            # Convert node_stack from level order to reverse level order
+            #node_stack = node_stack[::-1]
+
+            # Use reverse level order traversal to build function
+            node_results = {}   # Used to keep track of IR results by node ID
+            func_locals = {}    # Used to keep track of local variable names by node tag
+
+            bin_ops = ['=', '+', '-', '*', '/', '%', '&&', '||', '<', '>', "<=",
+                '>=', '==']
+            una_ops = ['++', '--', '+=', '-=', '*=', '/=', '<<', '>>']
+
+            while (len(node_stack) > 0):
+
+                iter_node = node_stack.pop()
+
+                if (iter_node.tag == "func_body"):
+                    # First node in subtree
+                    pass
+                elif (iter_node.tag in bin_ops):
+                    child_l = ast.children(iter_node.identifier)[0].identifier
+                    child_r = ast.children(iter_node.identifier)[1].identifier
+
+                    operand_l = node_results[child_l]
+                    operand_r = node_results[child_r]
+
+                    result = None
+
+                    if (iter_node.tag == '='):
+                        if (iter_node.tag in [p[1] for p in func_params]):
+                            # Assigning new value to function parameter
+                            print("Add case for assigning value to parameter")
+                            pass
+                        elif (iter_node.tag in func_locals):
+                            # Assigning new value to existing local variable
+                            print("Add case for assigning value to variable")
+                            pass
+                        else:
+                            # Defining new local variable
+                            print("Add case for defining new local variable")
+                            pass
+
+                    elif (iter_node.tag == '+'):
+                        result = builder.fadd(operand_l, operand_r)
+
+                    elif (iter_node.tag == '-'):
+                        result = builder.fsub(operand_l, operand_r)
+                        pass
+
+                    elif (iter_node.tag == '*'):
+                        result = builder.fmul(operand_l, operand_r)
+                        pass
+
+                    elif (iter_node.tag == '/'):
+                        result = builder.fdiv(operand_l, operand_r)
+                        pass
+
+                    elif (iter_node.tag == '%'):
+                        result = builder.frem(operand_l, operand_r)
+                        pass
+
+                    elif (iter_node.tag == '&&'):
+                        pass
+
+                    elif (iter_node.tag == '||'):
+                        pass
+
+                    elif (iter_node.tag == '<'):
+                        pass
+
+                    elif (iter_node.tag == '>'):
+                        pass
+
+                    elif (iter_node.tag == '<='):
+                        pass
+
+                    elif (iter_node.tag == '>='):
+                        pass
+
+                    elif (iter_node.tag == '=='):
+                        pass
+
+                    assert(result is not None)
+                    node_results[iter_node.identifier] = result
+
+                elif (iter_node.tag in una_ops):
+                    print("TO DO: Support for unary operators")
+                    pass
+
+                elif (iter_node.tag == 'return'):
+                    if (len(ast.children(iter_node.identifier)) == 0):
+                        # Void return
+                        builder.ret_void()
+                    else:
+                        # Returning an expression
+                        child_id = ast.children(iter_node.identifier)[0].identifier
+                        result = node_results[child_id]
+                        builder.ret(result)
+                else:
+                    # Node is either a constant, variable ID, or function call
+                    if (iter_node.tag in ir_funcs):
+                        # Function call
+                        arg_list = []
+                        for child in ast.children(iter_node.identifier):
+                            arg_list.append(node_results[child.identifier])
+
+                        builder.call(ir_funcs[iter_node.tag], arg_list)
+                        
+                    else:
+                        # Constant or variable
+                        assert(len(ast.children(iter_node.identifier)) == 0)
+
+                        var_type = ""
+                        is_numeral = iter_node.tag.isdigit()    # int
+                        if (is_numeral):
+                            var_type = "int"
+                        else:
+                            try:
+                                float(iter_node.tag)
+                                var_type = "float"
+                                is_numeral = True
+                            except:
+                                pass
+                            
+
+                        if (is_numeral):
+                            # Constant
+                            assert(len(var_type) > 0)
+                            assert(type_dict[var_type] is not None)
+                            
+                            constant = 0
+                            if (var_type == "int"):
+                                constant = int(iter_node.tag)
+                            elif (var_type == "float"):
+                                constant = float(iter_node.tag)
+                            else:
+                                print("Unaccounted for type")
+
+                            result = ir.Constant(type_dict[var_type], constant)
+
+                        else:
+                            # Variable
+                            if (iter_node.tag in [p[1] for p in func_params]):
+                                # Function argument
+                                result = function.args[
+                                    [p[1] for p in func_params].index(iter_node.tag)
+                                ]
+                            else:
+                                # Local variable
+                                result = func_locals[iter_node.tag]
+
+                    node_results[iter_node.identifier] = result
 
         elif (True):
             print("Handle case for global variables")
