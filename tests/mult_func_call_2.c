ac0a6e91db6e502643865b028fdfd85b2688707d
Hannah Murphy <han.jan.murphy@gmail.com>
2020-04-29 00:30:27 -0600

    Added new func call to mult_func_call_2

--- tests/mult_func_call_2.c
+++ tests/mult_func_call_2.c
@@ -1,7 +1,17 @@
-int func_dec(int a, int b, int c)
+int func_dec(int a, int b, int c, int d)
 {
-  int d = a + b + c;
-  return d;
+  int ef = a / b + c + d;
+  return ef;
+}
+
+int func_dec_2(int a, int b, int c, int aa, int ba, int ca, int as, int bs, int cs)
+{
+	return 0;
+}
+
+int func_dec_3()
+{
+  return 0;
 }
 
 int main(void)
@@ -9,8 +19,10 @@ int main(void)
   int a = 2 + 1;
   int x;
   int y;
+  a = func_dec(1, 2, 3, 4);
+  a = func_dec_2(1, 2, 3, 4, 5, 6, 7, 8, 9);
   x = 2;
   y = 3;
-  
+  y = func_dec_3();
   return 0;
 }
